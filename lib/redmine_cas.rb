9a616fdd513336a1d03f0382fee6ada0d8bd984c
Mirek Rusin <mirek@me.com>
2010-05-06 10:37:16 +0200

    Plugin configurable from Administration panel (Administration -> Plugins -> Configure)

--- lib/redmine_cas.rb
+++ lib/redmine_cas.rb
@@ -1,84 +1,144 @@
+require 'httpclient'
 require 'casclient'
 require 'casclient/frameworks/rails/filter'
 
-unless Setting.respond_to?(:cas_server_url)
-
-  Redmine::Plugin.register :redmine_cas do
-    name        "Redmine CAS plugin [disabled]"
-    author      'Mirek'
-    description "Redmine CAS authentication. To enable this plugin define 'cas_server_url' setting."
-    version     '0.0.1'
-  end
-
-else
-
-  Redmine::Plugin.register :redmine_cas do
-    name        "Redmine CAS plugin"
-    author      'Mirek Rusin <mirek [at] me [dot] com>'
-    description "Redmine CAS authentication (#{Setting.cas_server_url})"
-    version     '0.0.1'
-    #menu        :account_menu, :cas_sign_in, { :controller => 'account', :action => 'cas_login' }, :caption => 'CAS sign in'
-    
-    settings :default => {
-      'tab_text' => '',
-      'tab_name' => 'Tab Name',
-      'system_tab_text' => '',
-      'system_tab_name' => 'System Tab Name',
-      :cas => {
-        :server           => '',
-        :autocreate_users => false
-      }
-    }, :partial => 'settings/redminetab_settings'
-    
-  end
+Redmine::Plugin.register :redmine_cas do
+  name        "Redmine CAS plugin"
+  author      'Mirek Rusin'
+  description "Redmine CAS authentication"
+  version     '0.0.2'
   
-  CASClient::Frameworks::Rails::Filter.configure(
-    :cas_base_url => Setting.cas_server_url
-  )
-
-  ActionController::Dispatcher.to_prepare do
+  menu        :account_menu,
+              :login_without_cas,
+              {
+                :controller => 'account',
+                :action     => 'login_without_cas'
+              },
+              :caption => 'Sign in without CAS',
+              :after   => :login
   
-    AccountController.class_eval do
+  settings :default => {
+    :enabled      => false,
+    :cas_base_url => 'https://localhost'
+  }, :partial => 'settings/settings'
+  
+end
+
+class RedmineCas
+  
+  class << self
     
-      def login_with_cas
-        if params[:username].blank? && params[:password].blank? && Setting.respond_to?(:cas_server_url)
-          if session[:user_id]
+    def client_config
+      CASClient::Frameworks::Rails::Filter.config
+    end
+  
+    def plugin
+      Redmine::Plugin.find(:redmine_cas)
+    end
+    
+    # Get plugin setting value or it's default value in a safe way.
+    # If the setting key is not found, returns nil.
+    def get_setting(name)
+      if Setting["plugin_#{plugin.id}"]
+        Setting["plugin_#{plugin.id}"][name]
+      else
+        if plugin.settings[:default].has_key?(name)
+          plugin.settings[:default][name]
+        end
+      end
+    end
+  
+    # Update CAS configuration using settings.
+    # Has to be run at least once and can be run more times (after plugin configuration updates).
+    def configure_client!
+      CASClient::Frameworks::Rails::Filter.configure(
+        :cas_base_url => RedmineCas.get_setting(:cas_base_url)
+      )
+    end
+    
+    # Check if CAS client has the same config as plugin
+    def client_config_up_to_date?
+      client_config && client_config[:cas_base_url] == plugin.settings['cas_base_url']
+    end
+    
+    # Is CAS enabled, client configured and server available
+    def ready?
+      get_setting(:enabled) && server_reachable_by_client?
+    end
+    
+    # Check if server at configured url is alive
+    def server_reachable_by_client?
+      begin
+        if url = client_config[:cas_base_url]
+          if @server_available_for_url[url]
             true
           else
-            if CASClient::Frameworks::Rails::Filter.filter(self)
-              if user = User.find_by_login(session[:cas_user])
-                session[:user_id] = user.id
-                user_setup
-                redirect_to :controller => 'my', :action => 'page'
-              else
-                unless flash[:error]
-                  flash[:error] = l(:cas_authenticated_user_not_found, session[:cas_user])
-                  redirect_to :controller => params[:controller],
-                              :action     => params[:action],
-                              :back_url   => params[:back_url]
-                end
-              end
+            if !HTTPClient.new.get_content(url).empty?
+              @server_available_for_url[url] = true
+            else
+              false
             end
           end
         else
-          login_without_cas
+          false
         end
+#      rescue
+#        false
       end
-    
-      alias_method_chain :login, :cas
-    
-      def logout_with_cas
-        if Setting.respond_to?(:cas_server_url)
-          logout_user
-          CASClient::Frameworks::Rails::Filter.logout(self, home_url)
-        else
-          logout_without_cas
-        end
-      end
-    
-      alias_method_chain :logout, :cas
-    
     end
+    
+  end
+  
+end
+
+# Let's use dispatcher to setup CAS.
+# This way we can use it in development (executed on every reload) and production (executed on first reload only)
+# without worrying about plugins not being reloaded.
+ActionController::Dispatcher.to_prepare do
+
+  unless RedmineCas.client_config_up_to_date?
+    RedmineCas.configure_client!
+  end
+  
+  AccountController.class_eval do
+  
+    def login_with_cas
+      if params[:username].blank? && params[:password].blank? && RedmineCas.ready?
+        if session[:user_id]
+          true
+        else
+          if CASClient::Frameworks::Rails::Filter.filter(self)
+            if user = User.find_by_login(session[:cas_user])
+              session[:user_id] = user.id
+              user_setup
+              redirect_to :controller => 'my', :action => 'page'
+            else
+              unless flash[:error]
+                flash[:error] = l(:cas_authenticated_user_not_found, session[:cas_user])
+                redirect_to :controller => params[:controller],
+                            :action     => params[:action],
+                            :back_url   => params[:back_url]
+              end
+            end
+          end
+        end
+      else
+        login_without_cas
+      end
+    end
+    
+    alias_method_chain :login, :cas
+    
+    def logout_with_cas
+      if session[:cas_user]
+        CASClient::Frameworks::Rails::Filter.logout(self, home_url)
+        logout_user
+      else
+        logout_without_cas
+      end
+    end
+  
+    alias_method_chain :logout, :cas
   
   end
 
